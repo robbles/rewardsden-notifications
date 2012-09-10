712d05856ceed4223111bee964cbadd31fba7ca5
robbles <odwyerrob@gmail.com>
2012-09-10 15:38:34 -0700

    add send_message.py

---
+++ send_message.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from sys import argv
+from urllib import quote_plus
+from os import system
+
+if(len(argv) != 4):
+    print 'usage: send_message.py rUserId message points'
+    exit(1)
+
+user, message, points = argv[1:]
+user = int(user)
+points = int(points)
+text = quote_plus(message)
+
+system('curl -v -G rewardsden.com:8081 -dtext="{text}" -dpoints={points} -did={user}'.format(text=text, points=points, user=user))
+
